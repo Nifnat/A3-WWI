fb9c23c17de6c3d75956ec139b610bd03f9b289e
smackymander <smackymander@gmail.com>
2021-01-22 16:14:20 +0000

    Added bootstrap navbar and imported bootstrap css

--- frontEnd/a3-wwi-fe/src/App.js
+++ frontEnd/a3-wwi-fe/src/App.js
@@ -1,4 +1,5 @@
 import {BrowserRouter, Switch, Route, Link} from "react-router-dom"
+import {Navbar, NavbarBrand, NavLink} from "react-bootstrap"
 import Home from "./pages/Home"
 import AdminRoles from "./pages/AdminRoles"
 import AdminWeb from "./pages/AdminWeb"
@@ -7,19 +8,11 @@ import './App.css';
 function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <ul>
-          <li>
-            <Link to ={"/"} className = "nav-link"> Home </Link>
-          </li>
-          <li>
-            <Link to ={"/AdminRoles"} className = "nav-link"> Home </Link>
-          </li>
-          <li>
-            <Link to ={"/AdminWeb"} className = "nav-link"> Home </Link>
-          </li>
-        </ul>
-      </nav>
+      <Navbar bg="light">
+        <NavbarBrand><Link to ={"/"} className = "nav-link"> Home </Link></NavbarBrand>
+        <NavLink><Link to ={"/AdminRoles"} className = "nav-link"> Admin Roles </Link></NavLink>
+        <NavLink><Link to ={"/AdminWeb"} className = "nav-link"> Admin Web </Link></NavLink>
+      </Navbar>
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/AdminRoles' component={AdminRoles} />
