6d65843ffc6381708e0790783a9a413b112fc139
smackymander <smackymander@gmail.com>
2021-01-23 15:19:35 +0000

    Added drop down selecter for users

--- frontEnd/a3-wwi-fe/src/pages/AdminRoles.js
+++ frontEnd/a3-wwi-fe/src/pages/AdminRoles.js
@@ -1,11 +1,52 @@
+import { DropdownButton } from "react-bootstrap";
+import DropdownItem from "react-bootstrap/esm/DropdownItem";
+import React, { useState, useEffect } from "react";
 
+function AdminRoles() {
+  const [selectedUser, setSelectedUser] = useState("Select User");
+  const [usersList, setUsersList] = useState([]);
 
-function AdminRoles ()  {
-    return(
-        <div>
-            <h1>Admin Roles</h1>
-        </div>
-    )
+  useEffect(() => {
+    let apiResponse = doApiCall();
+    if (apiResponse.statusCode === 200) {
+      setUsersList(apiResponse.body);
+    } else {
+      setUsersList([]);
+    }
+  }, []);
+
+  function doApiCall() {
+    return {
+      statusCode: 200,
+      body: [
+        { name: "Jeff", steamID: "01234456789" },
+        { name: "Lilly", steamID: "09876543210" },
+      ],
+    };
+  }
+
+  function selectUser(user) {
+    if (user.name === selectedUser) {
+      setSelectedUser("Select User");
+    } else {
+      setSelectedUser(user.name);
+    }
+  }
+
+  return (
+    <div>
+      <h1>Admin Roles</h1>
+      <DropdownButton title={selectedUser}>
+        {usersList.map((user) => {
+          return (
+            <DropdownItem onClick={() => selectUser(user)}>
+              {user.name}
+            </DropdownItem>
+          );
+        })}
+      </DropdownButton>
+    </div>
+  );
 }
 
-export default AdminRoles
+export default AdminRoles;
