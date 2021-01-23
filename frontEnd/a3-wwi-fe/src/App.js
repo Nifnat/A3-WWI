339ec76db31128c9f6a7f13a65d83e9095c7d7aa
smackymander <smackymander@gmail.com>
2021-01-23 17:01:49 +0000

    Prettier on app.js

--- frontEnd/a3-wwi-fe/src/App.js
+++ frontEnd/a3-wwi-fe/src/App.js
@@ -1,25 +1,39 @@
-import {BrowserRouter, Switch, Route, Link} from "react-router-dom"
-import {Navbar, NavbarBrand, NavLink} from "react-bootstrap"
-import Home from "./pages/Home"
-import AdminRoles from "./pages/AdminRoles"
-import AdminWeb from "./pages/AdminWeb"
-import './App.css';
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { Navbar, NavbarBrand, NavLink } from "react-bootstrap";
+import Home from "./pages/Home";
+import AdminRoles from "./pages/AdminRoles";
+import AdminWeb from "./pages/AdminWeb";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar bg="light">
-        <NavbarBrand><Link to ={"/"} className = "nav-link"> Home </Link></NavbarBrand>
-        <NavLink><Link to ={"/AdminRoles"} className = "nav-link"> Admin Roles </Link></NavLink>
-        <NavLink><Link to ={"/AdminWeb"} className = "nav-link"> Admin Web </Link></NavLink>
+        <NavbarBrand>
+          <Link to={"/"} className="nav-link">
+            {" "}
+            Home{" "}
+          </Link>
+        </NavbarBrand>
+        <NavLink>
+          <Link to={"/AdminRoles"} className="nav-link">
+            {" "}
+            Admin Roles{" "}
+          </Link>
+        </NavLink>
+        <NavLink>
+          <Link to={"/AdminWeb"} className="nav-link">
+            {" "}
+            Admin Web{" "}
+          </Link>
+        </NavLink>
       </Navbar>
       <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/AdminRoles' component={AdminRoles} />
-        <Route exact path='/AdminWeb' component={AdminWeb} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/AdminRoles" component={AdminRoles} />
+        <Route exact path="/AdminWeb" component={AdminWeb} />
       </Switch>
     </BrowserRouter>
-  
   );
 }
 
